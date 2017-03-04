5c47e7ddb7f9a3a6741a6a1a57b666b987df623b
marze <marze@DESKTOP-6V1SRMO>
2017-03-04 11:58:12 +0100

    change

--- RTTI/RTTI/Source.cpp
+++ RTTI/RTTI/Source.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 #include "zwierz.h"
+#include "chomik.h"
+#include "kot.h"
+#include "pies.h"
 
 using namespace std;
 
---
+++ RTTI/RTTI/pies.h
@@ -0,0 +1,8 @@
+#pragma once
+#include <iostream>
+
+using namespace std;
+
+class Pies : public Zwierz {
+	void daj_glos();
+};
