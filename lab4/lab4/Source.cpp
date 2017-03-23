5b79ce0171624b21080b0fbad42f43bb512e52bc
Student <Student@S526-LAB03>
2017-03-23 13:54:43 +0100

    ssss

---
+++ lab4/lab4/Source.cpp
@@ -0,0 +1,27 @@
+/*#include<iostream>
+#include<string>
+#include<list>
+#include<vector>
+#include<map>
+#include<deque>
+#include<set>
+#include<array>
+#include<chrono>
+
+using namespace std;
+
+int main() {
+	const int ile = 5;
+	list<string>lista_slow;
+
+	string slowo;
+
+	for (int i = 0; i < ile; i++) {
+		cin >> slowo;
+		lista_slow.push_back(slowo);
+
+	}
+
+	system("pause");
+	return;
+}*/
